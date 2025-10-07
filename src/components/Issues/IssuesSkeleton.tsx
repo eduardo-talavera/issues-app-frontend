999518e369703d0eb1f6dfee0bc1a5aa0fa7eeb1
Eduardo Talavera <edutalavera@outlook.com>
2025-10-06 21:26:10 -0600

    feat(issues): issues skeleton loader improves added

--- src/components/Issues/IssuesSkeleton.tsx
+++ src/components/Issues/IssuesSkeleton.tsx
@@ -4,24 +4,24 @@ export const IssuesSkeleton = () => {
   return (
     <div className="animate-pulse px-8 py-6">
       {/* Header */}
-      <div className="flex justify-between items-center mb-8">
+      <div className="flex flex-col md:flex-row justify-between md:items-center mb-8">
         <div>
           <div className="h-8 w-40 bg-gray-300 rounded mb-2" />
           <div className="h-4 w-60 bg-gray-200 rounded" />
         </div>
-        <div className="h-10 w-32 bg-gray-300 rounded" />
+        <div className="h-10 w-32 bg-gray-300 rounded mt-5 md:mt-0" />
       </div>
 
       {/* Filtros */}
-      <div className="flex gap-4 mb-10">
-        <div className="h-10 w-60 bg-gray-200 rounded" />
-        <div className="h-10 w-40 bg-gray-200 rounded" />
-        <div className="h-10 w-40 bg-gray-200 rounded" />
+      <div className="flex flex-col md:flex-row gap-4 mb-10">
+        <div className="h-10 w-100 md:w-60 bg-gray-200 rounded" />
+        <div className="h-10 w-100 md:w-40 bg-gray-200 rounded" />
+        <div className="h-10 w-100 md:w-40 bg-gray-200 rounded" />
         <div className="h-10 w-28 bg-gray-300 rounded" />
       </div>
 
       {/* Columnas del tablero */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-[960px]">
         {[1, 2, 3].map((col) => (
           <div
             key={col}
@@ -31,7 +31,7 @@ export const IssuesSkeleton = () => {
             {[1, 2, 3].map((card) => (
               <div
                 key={card}
-                className="p-4 bg-white rounded-lg shadow-md flex flex-col gap-3"
+                className="p-4 bg-white h-[180px] w-100 rounded-lg shadow-md flex flex-col gap-3"
               >
                 <div className="h-4 w-3/4 bg-gray-200 rounded" />
                 <div className="h-3 w-2/3 bg-gray-100 rounded" />
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,6 @@
 
 export const Logo = () => {
   return (
-    // <img src="/logo.svg" alt="Logotipo UpTask" />
     <span className="text-3xl font-bold">TicketsApp</span>
   )
 }
